4eedba107e3a046e4aaebe27887b05e209896c4c
Iman Kianian <iman.kianian98@gmail.com>
2023-02-04 19:04:29 +0330

    Add Coil

--- app/src/main/java/com/example/samplegithubapp/base/SampleGitHubApp.kt
+++ app/src/main/java/com/example/samplegithubapp/base/SampleGitHubApp.kt
@@ -1,8 +1,16 @@
 package com.example.samplegithubapp.base
 
 import android.app.Application
+import coil.ImageLoaderFactory
+import coil.ImageLoader
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SampleGitHubApplication: Application() {
+class SampleGitHubApplication: Application(), ImageLoaderFactory {
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .crossfade(true)
+            .build()
+    }
 }
