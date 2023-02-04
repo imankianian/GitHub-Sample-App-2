e5fb972ece1a5fb28cc843172cd6012a78bffac5
Iman Kianian <iman.kianian98@gmail.com>
2023-02-04 12:10:50 +0330

    Add Hilt

---
+++ app/src/main/java/com/example/samplegithubapp/base/SampleGitHubApp.kt
@@ -0,0 +1,8 @@
+package com.example.samplegithubapp.base
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SampleGitHubApplication: Application() {
+}
