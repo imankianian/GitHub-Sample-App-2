8982bacf377f47a1321c00e31a4f32df7d8a9424
Iman Kianian <iman.kianian98@gmail.com>
2023-02-07 16:54:56 +0330

    Add soft caching

--- app/src/main/java/com/example/samplegithubapp/base/SampleGitHubApp.kt
+++ app/src/main/java/com/example/samplegithubapp/base/SampleGitHubApp.kt
@@ -3,6 +3,7 @@ package com.example.samplegithubapp.base
 import android.app.Application
 import coil.ImageLoaderFactory
 import coil.ImageLoader
+import coil.disk.DiskCache
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -10,6 +11,12 @@ class SampleGitHubApplication: Application(), ImageLoaderFactory {
 
     override fun newImageLoader(): ImageLoader {
         return ImageLoader.Builder(this)
+            .diskCache(
+                DiskCache.Builder()
+                    .directory(this.cacheDir.resolve("images_cache"))
+                    .maxSizePercent(0.1)
+                    .build()
+            )
             .crossfade(true)
             .build()
     }
